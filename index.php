84ceb2f7549ecb97903039406d2d0c9fcbb7301e
trooile <augusto.felipe83@gmail.com>
2021-03-09 15:22:56 -0300

    remove video

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php include "include/include.php";
 
-if(php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
-}
+// if(php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+//     return false;
+// }
 ?>
 <!DOCTYPE html>
 <html>
     <body>
-        <div class="container">
-            <video id="video" controls preload="auto" width="640" height="360" 
-                poster="poster.png">
-                <source src="public/stream.php?ext=webm" type='video/webm' />
+        <!-- <div class="container">
+            <video id="video" controls preload="auto" width="640" height="360" poster="../images/logo.png">
                 <source src="public/stream.php?ext=mp4" type='video/mp4' />
             </video>
-        </div>
+        </div> -->
     </body>
 </html>
 
-<?php include "include/footer.php" ?><samp></samp>
+<?php include "include/footer.php" ?>
--- public/stream.php
+++ public/stream.php
@@ -1,77 +1,60 @@
 <?php
 
-if (!isset($_GET['ext']) || $_GET['ext'] == 'mp4') {
- $path = dirname(__FILE__) . '../resource/video1.mp4';
-} else if ($_GET['ext'] == 'webm') {
- $path = dirname(__FILE__) . '../resource/video1.webm';
-} else {
- header('HTTP/1.1 400 Bad Request');
- return;
+if(!isset($_GET['ext']) || $_GET['ext'] == 'mp4') {
+    $path = dirname(__FILE__) . '../resource/video1.mp4';
+}else{
+    header('HTTP/1.1 400 Bad Request');
+    return;
 }
- 
-// Determina o mimetype do arquivo
-$finfo = new finfo(FILEINFO_MIME);
-$mime = $finfo->file($path);
- 
-// Define o tipo de conteúdo da resposta
-header('Content-type: ' . $mime);
- 
-// Tamanho do arquivo
+
+$file = new finfo(FILEINFO_MIME);
+$mp4 = $file->file($path);
+
+header('Content-type: ' . $mp4);
+
 $size = filesize($path);
- 
-//Verifica se foi passado o cabeçalho Range
-if (isset($_SERVER['HTTP_RANGE'])) {
-    // Parse do valor do campo
+
+if(isset($_SERVER['HTTP_RANGE'])) {
+    
     list($specifier, $value) = explode('=', $_SERVER['HTTP_RANGE']);
- 
-    //Tratamos apenas o especificador de range "bytes"
-    if ($specifier != 'bytes') {
+
+    if($specifier != 'bytes') {
         header('HTTP/1.1 400 Bad Request');
         return;
     }
- 
-    // Determina os bytes de início/fim
+
     list($from, $to) = explode('-', $value);
-    if (!$to) {
+    if(!$to) {
         $to = $size - 1;
     }
- 
-    // Cabeçalho da resposta
+
     header('HTTP/1.1 206 Partial Content');
     header('Accept-Ranges: bytes');
- 
-    // Tamanho da resposta
+
     header('Content-Length: ' . ($to - $from));
- 
-    // Bytes enviados na resposta
+
     header("Content-Range: bytes {$from}-{$to}/{$size}");
- 
-    // Abre o arquivo no modo bináro
+
     $fp = fopen($path, 'rb');
-    $chunkSize = 8192; // Tamanho dos blocos de leitura
- 
-    // Avança até o primeiro byte solicitado
+    $chunk = 8192; 
+
     fseek($fp, $from);
- 
-    // Manda os dados
+
     while(true){
-        // Verifica se já chegou ao byte final
+
         if(ftell($fp) >= $to){
             break;
         }
- 
-        // Envia o conteúdo
-        echo fread($fp, $chunkSize);
- 
-        // Flush do buffer
+
+        echo fread($fp, $chunk);
+
         ob_flush();
         flush();
     }
-} else {
-    // Se não possui o cabeçalho Range, envia todo o arquivo
+}else{
+
     header('Content-Length: ' . $size);
- 
-    // Lê o arquivo
+
     readfile($path);
 }
 ?>
