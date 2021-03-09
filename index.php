c0987b5146116f5886a48b59a5456da8219214e8
trooile <augusto.felipe83@gmail.com>
2021-03-09 14:59:26 -0300

    add video

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php include "include/include.php" ?>
+<?php include "include/include.php";
 
-<?php include "include/footer.php" ?>
+if(php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="container">
+            <video id="video" controls preload="auto" width="640" height="360" 
+                poster="poster.png">
+                <source src="stream.php?ext=webm" type='video/webm' />
+                <source src="stream.php?ext=mp4" type='video/mp4' />
+            </video>
+        </div>
+    </body>
+</html>
+
+<?php include "include/footer.php" ?><samp></samp>
---
+++ public/stream.php
@@ -0,0 +1,77 @@
+<?php
+//Determina o caminho do arquivo de acordo com a extensão
+if (!isset($_GET['ext']) || $_GET['ext'] == 'mp4') {
+ $path = dirname(__FILE__) . '/../resource/video1.mp4';
+} else if ($_GET['ext'] == 'webm') {
+ $path = dirname(__FILE__) . '/../resource/video1.webm';
+} else {
+ header('HTTP/1.1 400 Bad Request');
+ return;
+}
+ 
+// Determina o mimetype do arquivo
+$finfo = new finfo(FILEINFO_MIME);
+$mime = $finfo->file($path);
+ 
+// Define o tipo de conteúdo da resposta
+header('Content-type: ' . $mime);
+ 
+// Tamanho do arquivo
+$size = filesize($path);
+ 
+//Verifica se foi passado o cabeçalho Range
+if (isset($_SERVER['HTTP_RANGE'])) {
+    // Parse do valor do campo
+    list($specifier, $value) = explode('=', $_SERVER['HTTP_RANGE']);
+ 
+    //Tratamos apenas o especificador de range "bytes"
+    if ($specifier != 'bytes') {
+        header('HTTP/1.1 400 Bad Request');
+        return;
+    }
+ 
+    // Determina os bytes de início/fim
+    list($from, $to) = explode('-', $value);
+    if (!$to) {
+        $to = $size - 1;
+    }
+ 
+    // Cabeçalho da resposta
+    header('HTTP/1.1 206 Partial Content');
+    header('Accept-Ranges: bytes');
+ 
+    // Tamanho da resposta
+    header('Content-Length: ' . ($to - $from));
+ 
+    // Bytes enviados na resposta
+    header("Content-Range: bytes {$from}-{$to}/{$size}");
+ 
+    // Abre o arquivo no modo bináro
+    $fp = fopen($path, 'rb');
+    $chunkSize = 8192; // Tamanho dos blocos de leitura
+ 
+    // Avança até o primeiro byte solicitado
+    fseek($fp, $from);
+ 
+    // Manda os dados
+    while(true){
+        // Verifica se já chegou ao byte final
+        if(ftell($fp) >= $to){
+            break;
+        }
+ 
+        // Envia o conteúdo
+        echo fread($fp, $chunkSize);
+ 
+        // Flush do buffer
+        ob_flush();
+        flush();
+    }
+} else {
+    // Se não possui o cabeçalho Range, envia todo o arquivo
+    header('Content-Length: ' . $size);
+ 
+    // Lê o arquivo
+    readfile($path);
+}
+?>
