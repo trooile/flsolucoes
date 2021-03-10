684b011bde6141331b774840ac7e7c3ef08a6ec9
trooile <augusto.felipe83@gmail.com>
2021-03-10 16:28:34 -0300

    add fclink

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,4 @@
-<div
-	style='height: 40px; padding: 10px; text-align: center; font-size: 12px'>
+<div style='height: 40px; padding: 10px; text-align: center; font-size: 12px'>
 	© <?php echo date('Y')?> <a href='' class='fclink' target='new'>FL - Solutions</a>
 </div>
 
